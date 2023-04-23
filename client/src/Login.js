836bf2a9b1e4a2af5f09f740fd49eb1a2bebfb4c
ShaunFlood <shaunflood@hotmail.co.uk>
2023-04-23 18:22:20 +0100

    css for homepage

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { FaSpotify } from 'react-icons/fa';
+import './Login.css';
 
 function Login() {
+
   return (
     <div className="background">
-      <button onClick={() => window.location.href = 'http://localhost:8888/login'} className="btn btn-primary" style={{ backgroundColor: '#1db954', border: 'black'}}><FaSpotify /> Login with Spotify
-    </button>
+      <div className="login-container">
+        <h1 className="login-heading">Tempo Fit</h1>
+        <h5 className="login-undertext">Get in the rhythm of your day with our tempo-based playlist generator</h5>
+        <button id="login-but" onClick={() => window.location.href = 'http://localhost:8888/login'} className="btn btn-primary"><FaSpotify /> Login with Spotify</button>
+      </div>
     </div>
-    
   );
 }
 
--- client/src/dashboard.js
+++ client/src/dashboard.js
@@ -1,10 +1,8 @@
-import Profile from "./profile";
 import './dashboard.css'
 import Recommendations from "./recommendations";
 
 const Dashboard = () => {
   const videoPath = require('./starting.mp4')
-  const videoPath2 = require('./audiowave.mp4')
   
 return (
       <div>
@@ -22,7 +20,7 @@ return (
                 <h1 className="h1">Elevate your workouts with Music</h1>
                   <div>
                     <h3>How does it work?</h3>
-                    <p>Introducing TempoFit, the ultimate music discovery app powered by the Spotify API! TempoFit generates playlists based on the tempo on your favourite genres, allowing you to discover new music that matches your mood and energy level for your workouts. Simply enter a genre and average beats per minute you want the playlist to have, and TempoFit will create a custom playlist filled with tracks that will help elvate your workout.</p>
+                    <p>Introducing TempoFit, the ultimate music discovery app powered by the Spotify API! TempoFit generates playlists based on the tempo of your favourite genres, allowing you to discover new music that matches your mood and energy level for your workouts. Simply enter a genre and average beats per minute you want the playlist to have, and TempoFit will create a custom playlist filled with tracks that will help elvate your workout.</p>
                   </div>
               </div>
               <div className="gettingstarted">
@@ -31,9 +29,10 @@ return (
                 </video>
                   <div className="text-container">
                     <h3>Get started!</h3>
-                      <p>Enter the desired tempo in BPM, select genres for the songs, and specify the amount of songs, then let the app generate a playlist for you! </p>
+                      <p>Enter the desired tempo in BPM, select genres for the playlist, and specify the amount of songs that you want, then let the app generate a playlist for you! </p>
                       <p>You can enter multiple genres for some versitality in the playlist by just seperating each genre by a comma in the search field. An example of this is 'edm,pop,hip-hop'</p>
-                      <p>TempoFit will generate the songs by spotify next to the form. Where you can save the playlist to your own spotify account, so it can be used for your workouts.</p>
+                      <p>TempoFit will generate the songs by spotify next to the form.</p>
+                      <p>Where you can save the playlist to your own spotify account by clicking the save button, so it can be easiliy used for your workouts.</p>
                       <a href="#form" class="hero-cta" style={{backgroundColor: "#00adb5"}}>Start Generating</a>
                   </div>
               </div>
